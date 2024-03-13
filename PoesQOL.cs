62acbd7566a9764cc491566f3b1d310ad4dbe533
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-03-14 06:27:58 +1100

    Working Version
    Has
    - Gold Fever Scale Fix
    - Move Speed Cap
    - Settings Menu working

--- PoesQOL.cs
+++ PoesQOL.cs
@@ -1,12 +1,11 @@
 ﻿using HarmonyLib;
-using Il2CppVampireSurvivors.Framework;
+using Il2CppSystem;
 using Il2CppVampireSurvivors.Framework.NumberTypes;
 using Il2CppVampireSurvivors.Objects.Characters;
 using Il2CppVampireSurvivors.UI;
 using MelonLoader;
-
-using static Il2CppVampireSurvivors.UI.OptionsController;
-
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PoesQOL
 {
@@ -21,7 +20,6 @@ namespace PoesQOL
         public const string DownloadLink = "https://github.com/LeCloutPanda/PoesQOL";
     }
 
-
     public class PoesQOL : MelonMod
     {
         private static MelonPreferences_Category modCategory;
@@ -32,16 +30,17 @@ namespace PoesQOL
         private static MelonPreferences_Entry<float> moveSpeedMax;
         // Gold Fever Fix
         private static MelonPreferences_Entry<bool> goldFeverFixToggle;
-        // Golden Eggs for all
-        private static MelonPreferences_Entry<bool> allowGoldenEggsOnEveryone;
 
         public override void OnInitializeMelon()
         {
             modCategory = MelonPreferences.CreateCategory(ModInfo.Id);
+
             globalToggle = modCategory.CreateEntry<bool>("globalToggle", true);
+
             moveSpeedCapToggle = modCategory.CreateEntry<bool>("moveSpeedCapToggle", true);
             moveSpeedMin = modCategory.CreateEntry<float>("moveSpeedMin", -1.0f);
             moveSpeedMax = modCategory.CreateEntry<float>("moveSpeedMax", 3.0f);
+
             goldFeverFixToggle = modCategory.CreateEntry<bool>("goldFeverFixToggle", true);
         }
 
@@ -59,6 +58,45 @@ namespace PoesQOL
                 if (_MoveSpeed < moveSpeedMin.Value) _MoveSpeed = new EggFloat(moveSpeedMin.Value);
                 if (_MoveSpeed > moveSpeedMax.Value) _MoveSpeed = new EggFloat(moveSpeedMax.Value);
                 __instance.PlayerStats.MoveSpeed = _MoveSpeed;
+            }
+        }
+
+        [HarmonyPatch(typeof(GoldFeverUIManager), "OnEnable")]
+        public class GoldFeverUIManagerOnEnable_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(GoldFeverUIManager __instance)
+            {
+                // Toggle checks for global and local
+                if (!globalToggle.Value) return true;
+                if (!goldFeverFixToggle.Value) return true; __instance.IntroTween();
+                return false;
+            }
+        }
+
+
+        [HarmonyPatch(typeof(OptionsController), "BuildGameplayPage")]
+        public class CustomSettings
+        {
+            [HarmonyPostfix]
+            public static void Postfix(OptionsController __instance)
+            {
+                __instance.AddLabel("Poes QOL");
+                __instance.AddTickBox("Global Enable", modCategory.GetEntry<bool>(globalToggle.Identifier).Value, (Action<bool>)((value) => { globalToggle.Value = value; modCategory.SaveToFile(true); }), false);
+                __instance.AddTickBox("Move Speed Cap Enable", modCategory.GetEntry<bool>(moveSpeedCapToggle.Identifier).Value, (Action<bool>) ((value) => { moveSpeedCapToggle.Value = value; modCategory.SaveToFile(true); }), false);
+                //__instance.AddSlider("Move Speed Max", modCategory.GetEntry<float>(moveSpeedMax.Identifier).Value, (Action<float>) ((value) => { moveSpeedMax.Value = value; }), false, 0.0f, 10.0f);
+                __instance.AddTickBox("Gold Fever Scale Fix Enable", modCategory.GetEntry<bool>(goldFeverFixToggle.Identifier).Value, (Action<bool>) ((value) => { goldFeverFixToggle.Value = value; modCategory.SaveToFile(true); }), false);                
+            }
+        }
+
+        [HarmonyPatch("Il2CppInterop.HarmonySupport.Il2CppDetourMethodPatcher", "ReportException")]
+        public static class Patch_Il2CppDetourMethodPatcher
+        {
+            public static bool Prefix(Exception ex)
+            {
+                MelonLogger.Error("During invoking native->managed trampoline", ex);
+
+                return false;
             }
         }
     }
